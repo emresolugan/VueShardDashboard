25ccf226ee0e24e1a5d36623eda65397d3b2c8a3
Nurullah <nurullahturkyilmaz@outlook.com>
2020-05-13 14:57:26 +0300

    added vuex modifications.

--- src/pages/BlogDashboard/BlogDashboard.vue
+++ src/pages/BlogDashboard/BlogDashboard.vue
@@ -1,180 +1,212 @@
 <template>
-  <!--<PageHeader :title="this.title" :subTitle="this.subTitle"></PageHeader>-->
-
-   <div class="hello">
+  <div class="hello">
     <div class="card tableDiv">
       <div class="card-body cardBodyClass">
-        <button type="button" class="btn btn-primary createpdfBtn" v-on:click="createPDF()">Create PDF</button>
+        <button
+          type="button"
+          class="btn btn-primary createpdfBtn"
+          v-on:click="createPDF()"
+        >Create PDF</button>
         <button type="button" class="btn btn-primary pdfBtn" v-on:click="exportPDF()">Export PDF</button>
         <div class="input-group mb-3">
-            <input type="text" class="form-control searchBarClass" v-on:keyup="searchConsumers($event)" placeholder="Search Bar" aria-label="SearchBar"/>
+          <input
+            type="text"
+            class="form-control searchBarClass"
+            v-on:keyup="searchConsumers($event)"
+            placeholder="Search Bar"
+            aria-label="SearchBar"
+          />
         </div>
         <table class="table table-striped table-bordered clientTableClass" id="pdfScreen">
-            <thead>
-              <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Surname</th>
-                  <th scope="col">MobilePhone</th>
-                  <th scope="col">IP</th>
-                  <th scope="col">MAC</th>
-                  <th scope="col" colSpan="2" data-html2canvas-ignore="true" class="iconColClass"><i class="fa fa-plus-circle fa-2x addIconClass" v-on:click="routeFunc('/addblog')"></i></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in consumerList" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.surname }}</td>
-                <td>{{ item.mobilephone }}</td>
-                <td>{{ item.ip }}</td>
-                <td>{{ item.mac }}</td>
-                <td class="iconColClass" data-html2canvas-ignore="true"><i class="fa fa-edit fa-lg editIconClass" v-on:click="routeFunc('/addblog/' + item.id)"></i></td>
-                <td class="iconColClass" data-html2canvas-ignore="true"><i class="fa fa-minus-circle fa-lg deleteIconClass" v-on:click="deleteConsumer(item.id)"></i></td>
-              </tr>
-            </tbody>
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Surname</th>
+              <th scope="col">MobilePhone</th>
+              <th scope="col">IP</th>
+              <th scope="col">MAC</th>
+              <th scope="col" colspan="2" data-html2canvas-ignore="true" class="iconColClass">
+                <em class="fa fa-plus-circle fa-2x addIconClass" v-on:click="routeFunc('/addblog')"></em>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in consumerList" :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.surname }}</td>
+              <td>{{ item.mobilephone }}</td>
+              <td>{{ item.ip }}</td>
+              <td>{{ item.mac }}</td>
+              <td class="iconColClass" data-html2canvas-ignore="true">
+                <em
+                  class="fa fa-edit fa-lg editIconClass"
+                  v-on:click="routeFunc('/addblog/' + item.id)"
+                ></em>
+              </td>
+              <td class="iconColClass" data-html2canvas-ignore="true">
+                <em
+                  class="fa fa-minus-circle fa-lg deleteIconClass"
+                  v-on:click="deleteConsumer(item.id)"
+                ></em>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-import PageHeader from "@/components/main/PageHeader";
-import jspdf from 'jspdf' 
-import 'jspdf-autotable'
-import html2canvas from "html2canvas"
-import { GetAll } from '../../services/apicall';
-import { Delete } from '../../services/apicall';
+import PageHeader from "../../components/main/PageHeader";
+import jspdf from "jspdf";
+import "jspdf-autotable";
+import html2canvas from "html2canvas";
+import { GetAll } from "../../services/apicall";
+import { Delete } from "../../services/apicall";
 
 export default {
   name: "blog-dahboard",
   data() {
-      return {
-        consumerList: [],
-        copyConsumerList: []
-      };
+    return {
+      consumerList: [],
+      copyConsumerList: []
+    };
   },
   components: {
     PageHeader
   },
   methods: {
-    exportPDF: function () {
-      var data = document.getElementById('pdfScreen');  
-      html2canvas(data).then(canvas => { 
-    
-        const contentDataURL = canvas.toDataURL('image/png')  
-        const pdf = new jspdf('p','pt', 'a4'); 
-        const imgProps= pdf.getImageProperties(contentDataURL);
+    exportPDF: function() {
+      var data = document.getElementById("pdfScreen");
+      html2canvas(data).then(canvas => {
+        const contentDataURL = canvas.toDataURL("image/png");
+        const pdf = new jspdf("p", "pt", "a4");
+        const imgProps = pdf.getImageProperties(contentDataURL);
         const pdfWidth = pdf.internal.pageSize.getWidth();
         const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        pdf.text("Hello World", 260, 20)
-        var position = 50;  
-        pdf.addImage(contentDataURL, 'PNG', 0, position, pdfWidth, pdfHeight)
-        pdf.text("Hello World", 260, 290)
-        pdf.save('table.pdf'); // Generated PDF   
-      }); 
-    },
-
-    createPDF: function () {
-      var doc = new jspdf('p','pt', 'a4');
-
-      doc.text("Hello World", 260, 20)
-      doc.autoTable({ html: '#pdfScreen' })
-
-      doc.save('sample-file.pdf');
-    
-    },
-
-    getConsumers(){
-      let headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("accesstoken")};
-      GetAll("http://localhost:5000/api/User/GetAll", headers).then(response => {
-        if(typeof response == "number")
-        {
-          if(response == 401)
-          {
-            // route to login
-          }
-          else if(response == 403)
-          {
-            // do something
-          }
-        }
-        else
-        {
-          debugger;
-          this.consumerList = response.slice();
-          this.copyConsumerList = response.slice();
-        }
+        pdf.text("Hello World", 260, 20);
+        var position = 50;
+        pdf.addImage(contentDataURL, "PNG", 0, position, pdfWidth, pdfHeight);
+        pdf.text("Hello World", 260, 290);
+        pdf.save("table.pdf"); // Generated PDF
       });
-
     },
 
-    searchConsumers(event)
-    {
-      debugger // eslint-disable-line
-      if(event.target.value == "")
-      {
+    createPDF: function() {
+      var doc = new jspdf("p", "pt", "a4");
+
+      doc.text("Hello World", 260, 20);
+      doc.autoTable({ html: "#pdfScreen" });
+
+      doc.save("sample-file.pdf");
+    },
+
+    getConsumers() {
+      let headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("accesstoken")
+      };
+      GetAll("http://localhost:5000/api/User/GetAll", headers).then(
+        response => {
+          if (typeof response == "number") {
+            if (response == 401) {
+              // route to login
+            } else if (response == 403) {
+              // do something
+            }
+          } else {
+            debugger;
+            this.consumerList = response.slice();
+            this.copyConsumerList = response.slice();
+          }
+        }
+      );
+    },
+
+    searchConsumers(event) {
+      debugger; // eslint-disable-line
+      if (event.target.value == "") {
         this.consumerList = [];
         this.consumerList = this.copyConsumerList.slice();
-      }
-      else
-      {
+      } else {
         this.consumerList = [];
 
-        for(let i in this.copyConsumerList)
-        {
-          if(this.copyConsumerList[i].id.toString().toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+        for (let i in this.copyConsumerList) {
+          if (
+            this.copyConsumerList[i].id
+              .toString()
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
-          }
-          else if(this.copyConsumerList[i].name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+          } else if (
+            this.copyConsumerList[i].name
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
-          }
-          else if(this.copyConsumerList[i].surname.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+          } else if (
+            this.copyConsumerList[i].surname
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
-          }
-          else if(this.copyConsumerList[i].mobilephone.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+          } else if (
+            this.copyConsumerList[i].mobilephone
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
-          }
-          else if(this.copyConsumerList[i].ip.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+          } else if (
+            this.copyConsumerList[i].ip
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
-          }
-          else if(this.copyConsumerList[i].mac.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
-          {
+          } else if (
+            this.copyConsumerList[i].mac
+              .toLocaleLowerCase()
+              .includes(event.target.value.toLocaleLowerCase())
+          ) {
             this.consumerList.push(this.copyConsumerList[i]);
           }
         }
       }
     },
 
-    routeFunc: function (path) {
+    routeFunc: function(path) {
       // catch unhandled promise hatası için
       this.$router.push(path).catch(e => {});
     },
 
-    deleteConsumer(id){
+    deleteConsumer(id) {
       debugger;
-      let headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("accesstoken")};
+      let headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("accesstoken")
+      };
       Delete("http://localhost:5000/api/User/", id, headers)
-      .then(response => {
-        if(response == 200)
-         this.consumerList.splice(this.consumerList.findIndex(x => x.id == id), 1);
-      }).catch((error) => console.error(error))        
+        .then(response => {
+          if (response == 200)
+            this.consumerList.splice(
+              this.consumerList.findIndex(x => x.id == id),
+              1
+            );
+        })
+        .catch(error => console.error(error));
     }
-
   },
-  created: function (){
+  created: function() {
     this.getConsumers();
-  },
+  }
 };
 </script>
 
 <style>
- @import "BlogDashboard.css";
+@import "../../assets/styles/css/BlogDashboard.css";
 </style>
--- src/services/apicall.js
+++ src/services/apicall.js
@@ -1,161 +1,129 @@
-export async function GetAll (url, headers) {
+export async function GetAll(url, headers) {
+  let _data = {
+    method: "GET",
+    headers: headers
+  };
 
-    let _data = {
-        method: 'GET',
-        headers: headers
-    } 
+  let response = await fetch(url, _data);
 
-    let response = await fetch(url, _data);
-
-    if(response.status == 401)
-    {
-        if(localStorage.getItem("accesstoken") === null)
-        {
-            return response.status;
-        }
-        else
-        {
-            return 402;
-        }
+  if (response.status == 401) {
+    if (localStorage.getItem("accesstoken") === null) {
+      return response.status;
+    } else {
+      return 402;
     }
-    else if(response.status == 403)
-    {
-        return response.status;
-    }
-    else if(response.status == 200)
-    {
-        return response.json();
-    }
+  } else if (response.status == 403) {
+    return response.status;
+  } else if (response.status == 200) {
+    return response.json();
+  }
 }
 
-export async function GetWithID (url, id, headers) {
+export async function GetWithID(url, id, headers) {
+  let _data = {
+    method: "GET",
+    headers: headers
+  };
 
-    let _data = {
-        method: 'GET',
-        headers: headers
-    } 
+  debugger;
+  let response = await fetch(url + id, _data);
+  debugger;
 
-    debugger;
-    let response = await fetch(url + id, _data);
-    debugger;
-
-    if(response.status == 401)
-    {
-        if(localStorage.getItem("accesstoken") === null)
-        {
-            return response.status;
-        }
-        else
-        {
-            return 402; // yine bir authorization kodu componentte ayırt edileilsin diye
-        }
+  if (response.status == 401) {
+    if (localStorage.getItem("accesstoken") === null) {
+      return response.status;
+    } else {
+      return 402; // yine bir authorization kodu componentte ayırt edileilsin diye
     }
-    else if(response.status == 403)
-    {
-        return response.status
-    }
-    else if(response.status == 200)
-    {
-        return response.json();
-    }
+  } else if (response.status == 403) {
+    return response.status;
+  } else if (response.status == 200) {
+    return response.json();
+  }
 }
 
-export async function Post (url, item, headers) {
+export async function Post(url, item, headers) {
+  let _data = {
+    method: "POST",
+    body: JSON.stringify(item),
+    headers: headers
+  };
 
-    let _data = {
-        method: 'POST',
-        body: JSON.stringify(item),
-        headers: headers
-    } 
- 
-    let response = await fetch(url, _data);
+  let response = await fetch(url, _data);
 
-    if(response.status == 401)
-    {
-        if(localStorage.getItem("accesstoken") === null)
-        {
-            return response.status;
-        }
-        else
-        {
-            return 402;
-        }
+  if (response.status == 401) {
+    if (localStorage.getItem("accesstoken") === null) {
+      return response.status;
+    } else {
+      return 402;
     }
-    else
-    {
-        return response.status;
-    }
+  } else {
+    return response.status;
+  }
 }
 
-export async function Put (url, item, headers) {
-    let _data = {
-        method: 'PUT',
-        body: JSON.stringify(item),
-        headers: headers
-    } 
-    
-    let response = await fetch(url, _data);
+export async function Put(url, item, headers) {
+  let _data = {
+    method: "PUT",
+    body: JSON.stringify(item),
+    headers: headers
+  };
 
-    if(response.status == 401)
-    {
-        if(localStorage.getItem("accesstoken") === null)
-        {
-            return response.status;
-        }
-        else
-        {
-            return 402;
-        }
+  let response = await fetch(url, _data);
+
+  if (response.status == 401) {
+    if (localStorage.getItem("accesstoken") === null) {
+      return response.status;
+    } else {
+      return 402;
     }
-    else
-    {
-        return response.status;
-    }
+  } else {
+    return response.status;
+  }
 }
 
-export async function Delete (url, id, headers) {
-    debugger;
-    let _data = {
-        method: 'DELETE',
-        headers: headers
-    } 
-    
-    let response = await fetch(url + id, _data);
+export async function Delete(url, id, headers) {
+  debugger;
+  let _data = {
+    method: "DELETE",
+    headers: headers
+  };
 
-    if(response.status == 401)
-    {
-        if(localStorage.getItem("accesstoken") === null)
-        {
-            return response.status;
-        }
-        else
-        {
-            return 402;
-        }
+  let response = await fetch(url + id, _data);
+
+  if (response.status == 401) {
+    if (localStorage.getItem("accesstoken") === null) {
+      return response.status;
+    } else {
+      return 402;
     }
-    else
-    {
-        return response.status;
-    }
+  } else {
+    return response.status;
+  }
 }
 
-export async function refreshToken()
-{
-    let headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Ref_Tok': localStorage.getItem("refreshtoken"), 'Acc_Tok': localStorage.getItem("accesstoken")};
-    debugger;
-    let _data = {
-        method: 'POST',
-        headers: headers
-        }
-    
-   let response = await fetch("http://localhost:5000/api/Token/RefreshTokens/", _data);
-   debugger;
+export async function refreshToken() {
+  let headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Ref_Tok: localStorage.getItem("refreshtoken"),
+    Acc_Tok: localStorage.getItem("accesstoken")
+  };
+  debugger;
+  let _data = {
+    method: "POST",
+    headers: headers
+  };
 
-   if(response.status == 200)
-   {
-       return response.json();
-   }
+  let response = await fetch(
+    "http://localhost:5000/api/Token/RefreshTokens/",
+    _data
+  );
+  debugger;
 
+  if (response.status == 200) {
+    return response.json();
+  }
 }
 
 /*export function PostWithModel (url, item, headers) {
