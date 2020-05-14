b308709e04311254d168107ece72c6b874ff91c1
Nurullah <nurullahturkyilmaz@outlook.com>
2020-05-14 09:17:35 +0300

    fx

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import user from "./modules/user";
-import auth from "./modules/auth";
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+import tokenModule from './modules/tokenmodule'
 
-const debug = process.env.NODE_ENV !== "production";
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user,
-    auth
-  },
-  strict: debug
-});
+    tokenModule
+  }
+})
